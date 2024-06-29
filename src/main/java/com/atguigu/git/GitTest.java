f7a8617ae273b3000499e242def76466116117bd
峰 <106209110+2firenut@users.noreply.github.com>
2024-06-29 19:08:08 +0800

    Update GitTest.java
    MAN！WHAT CAN I SAY！

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,7 +2,7 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("MAN！WHAT CAN I SAY！");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
