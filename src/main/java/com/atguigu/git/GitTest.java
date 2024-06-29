03720140ef8f77c70d9353f52b985ce0e8abd37f
huangshaofeng602 <huangshaofeng602@hellobike.com>
2024-06-29 20:47:31 +0800

    2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("push local test");
         System.out.println("1");
+        System.out.println("2");
     }
 }
